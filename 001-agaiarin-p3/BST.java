97e486a44e5eb446b01f56595169b5c340f69943
Alessandro Gaiarin <agaiarin@gmail.com>
2022-03-25 16:04:14 -0400

    BST is written, but I'm having bugs. I think my tree is actually inserting new nodes properly, but currently isn't working with its toString() method, and possibly more.

--- 001-agaiarin-p3/BST.java
+++ 001-agaiarin-p3/BST.java
@@ -44,8 +44,24 @@ class BST<T extends Comparable<T>> {
 	 *  @return string in which the elements are listed in an inorder fashion
 	 */
 	public String inorderToString() {
-		// YOUR CODE GOES HERE
-		return "";
+		String returnString = inStringNode(root, "");
+		return returnString; //return the TRIMMED version of returnString to get rid of any trailing spaces.
+		//didn't know trim() was part of java.lang! that's nice.
+	}
+
+	/**
+	 * Private method for inorderToString().
+	 * @param node
+	 * @param returnString
+	 * @return
+	 */
+	private String inStringNode(Node<T> node, String returnString) {
+		if (node != null) {
+			inStringNode(node.left, returnString); //travel down the left subtree
+			returnString = returnString + "\"" + node + "\" "; //display the root
+			inStringNode(node.right, returnString); //travel down the right subtree
+		}
+		return returnString; //return the completed string
 	}
 	
 	/**
@@ -54,7 +70,33 @@ class BST<T extends Comparable<T>> {
 	 *  @param key to insert
 	 */
 	public void insert(T key) {
-		// YOUR CODE GOES HERE 
+		root = insertNode(root, key);
+	}
+
+	/**
+	 * Private helper function for insert().
+	 * @param node root, starting node of the BST.
+	 * @param key key that we want to add.
+	 * @return the newly added node with the new key.
+	 */
+	private Node<T> insertNode(Node<T> node, T key) {
+		if (node == null) { //creates new node at empty spot
+			node = new Node<T>(key);
+			return node;
+		}
+		//compareTo() reminder: (i literally do this every time lmao)
+		//	x.compareTo(y) returns:
+		//		- 0 if x == y
+		//		- value < 0 if x < y
+		//		- value > 0 if x > y
+		else if (key.compareTo(node.key) < 0) { //if the key is less than the current key's value...
+			node.left = insertNode(node.left, key); //it moves to the left
+		}
+		else if (key.compareTo(node.key) > 0){ //if the key is greater than the current key's value...
+			node.right = insertNode(node.right, key); //it moves to the right
+		}
+		return node; //finally, the node is returned
+
 	}
 	
 	/**
@@ -64,10 +106,31 @@ class BST<T extends Comparable<T>> {
 	 *  @param key to find
 	 *  @return the node associated with the key.
 	 */
-	public Node<T> find(T key)	{ 					
-		// YOUR CODE GOES HERE
-		return null;
-	}	 
+	public Node<T> find(T key)	{
+		return findNode(root, key);
+	}
+
+	/**
+	 * Private helper function for find().
+	 * @param node
+	 * @param key
+	 * @return
+	 */
+	private Node<T> findNode(Node<T> node, T key) {
+		if (node == null) {
+			return null;
+		}
+		if (key.compareTo(node.key) == 0) {
+			return node;
+		}
+
+		else if (key.compareTo(node.key) < 0) {
+			return findNode(node.left, key);
+		}
+		else {
+			return findNode(node.right, key);
+		}
+	}
 	
 
 	/**
@@ -88,13 +151,17 @@ class BST<T extends Comparable<T>> {
 		BST<Integer> tree1 = new BST<>();
 		tree1.insert(50); tree1.insert(30); tree1.insert(20); tree1.insert(40);
 		tree1.insert(70); tree1.insert(60); tree1.insert(80);
-		
+
+		System.out.println("Current tree: " + tree1.inorderToString());
+		System.out.println("Current root: " + tree1.root);
+
 		if (tree1.find(70) != null) {
 			System.out.println("Yay1");
 		}
 		if (tree1.find(90) == null) {
 			System.out.println("Yay2");
 		}
+		System.out.println(tree1.toString());
 		if (tree1.toString().equals("\"20\" \"30\" \"40\" \"50\" \"60\" \"70\" \"80\"") == true) {
 			System.out.println("Yay3");
 		}
